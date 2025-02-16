715522b9d1d57a05fe7c90a7f70b52bc2fc649ba
VStee1e <steelevw@rose-hulman.edu>
2025-02-16 13:39:37 -0500

    Adding descriptions and comments

--- ece230FinalProjectSteeleVWebbJ/ece230FinalProjectSteeleVWebbJmain.c
+++ ece230FinalProjectSteeleVWebbJ/ece230FinalProjectSteeleVWebbJmain.c
@@ -1,3 +1,16 @@
+/*  ECE230 Final Project main.c File
+ * Created by Vance Steele and John Webb
+ * Date: Feb 16, 2025
+ *
+ * Description: Uses ultrasonic sensor and interrupts to capture and trigger LCD screen,
+ *              speaker, and various LEDS, as well as turn a servo motor. A keypad is
+ *              then used with interrupts to disable the speaker and LEDs, while also
+ *              changing the LCD display and turning the servo motor back to its
+ *              starting position.
+ *
+ */
+
+
 #include "msp.h"
 
 //TEST COMMIT!!
